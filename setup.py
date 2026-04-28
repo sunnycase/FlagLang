4706f0875524e73e1c7a3feea607c67ab1860546
sunnycase <sunnycase@live.cn>
2026-04-28 08:09:08 +0000

    Fix packaging and ctest review issues

--- setup.py
+++ setup.py
@@ -660,6 +660,7 @@ def get_packages():
 
     if check_env_flag("TRITON_BUILD_PROTON", "ON"):  # Default ON
         yield "triton.profiler"
+        yield "triton.profiler.hooks"
 
 
 def add_link_to_backends(external_only):
